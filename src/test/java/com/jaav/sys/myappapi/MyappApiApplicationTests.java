9c4cfd9348b30110a077248940803692ce478aab
git-jaav <araucovillar@gmail.com>
2022-03-31 22:51:16 -0500

    fix test

--- src/test/java/com/jaav/sys/myappapi/MyappApiApplicationTests.java
+++ src/test/java/com/jaav/sys/myappapi/MyappApiApplicationTests.java
@@ -3,10 +3,12 @@ package com.jaav.sys.myappapi;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles(profiles = {"dev","local"})
 public class MyappApiApplicationTests {
 
 	@Test
